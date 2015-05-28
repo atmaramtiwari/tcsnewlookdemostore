9b1f5bf86807ada430730a60dbca7d97a148bf90
Atmaram <atmaram.tiwari@tcs.com>
2015-05-28 14:25:08 +0530

    commit for demo

---
+++ newlook/newlookcockpits/gensrc/com/tcs/cockpits/constants/GeneratedNewlookCockpitsConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at May 25, 2015 5:45:47 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.tcs.cockpits.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedNewlookCockpitsConstants
+{
+	public static final String EXTENSIONNAME = "newlookcockpits";
+	
+	protected GeneratedNewlookCockpitsConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
